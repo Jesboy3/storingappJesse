c6d363ac92ef45b870e321460bbe212db0997f05
Jesboy3 <D304279@edu.curio.nl>
2024-03-11 10:33:43 +0100

    dfs

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -2,33 +2,54 @@
 
 //Variabelen vullen
 $attractie = $_POST['attractie'];
-$capaciteit = $_POST['capaciteit'];
-$melder = $_POST['melder'];
-$type = $_POST['type'];
-if(isset($_POST['prioritiet']))
+if(empty($attractie))
 {
-    $prioriteit = true;
+    $errors[] = "Vul de attractie naam in.";
+}
+$type = $_POST['type'];
+$capaciteit = $_POST['capaciteit'];
+if(!is_numeric($capaciteit))
+{
+    $errors[] = "Vul voor capaciteit een geldig getal in";
+}
+if(isset($_POST['prioriteit']))
+{
+    $prioriteit = 0;
 }
 else
 {
-    $prioriteit = false;
+    $prioriteit = 1;
 }
+$melder = $_POST['melder'];
+if(empty($melder))
+{
+    $errors[] = "Vul de melders naam in."
+}
+$overig = $_POST['overig'];
 
 
 //1. Verbinding
 require_once '../../../config/conn.php';
 
 //2. Query
-$query="INSERT INTO meldingen(attractie, capaciteit, melder, type) VALUES(:Pietje, :Puk, :melder :type)";
+$query="INSERT INTO meldingen(attractie, type, capaciteit, prioriteit, melder, overige_info ) VALUES(:attractie, :type, :capaciteit, :prioriteit, :melder, :overige_info)";
 //3. Prepare
 $statement = $conn->prepare($query);
 //4. Execute
 $statement->execute([
-    ":Pietje"=> $attractie,
-    ":Puk"=> $capaciteit,
+    ":attractie"=> $attractie,
+    ":type"=> $type,
+    ":capaciteit"=> $capaciteit,
+    "prioriteit"=> $prioriteit,
     ":melder"=> $melder,
-    "type"=> $type,
-    
+    ":overige_info"=> $overig
  ]);
 
- header("Location: ../../../resources/views/meldingen/index.php?msg=Melding opgeslagen");   
+ header("Location: ../../../resources/views/meldingen/index.php?msg=Melding opgeslagen"); 
+ 
+ //5. Bij errors dumpen 
+ if(isset($errors))
+ {
+    var_dump($errors);die();
+}
+ 
--- resources/views/meldingen/create.php
+++ resources/views/meldingen/create.php
@@ -18,34 +18,44 @@
 
             <div class="form-group">
                 <label for="attractie">Naam attractie:</label>
-                <input type="text" name="attractie" id="attractie" class="form-input">
+                <input type="text" name="attractie" id="attractie" class="form-input" placeholder="Attractie">
             </div>
+            
             <div class="form-group">
                 <label for="type">Type</label>
-                <select type="text" name="type" id="group">
+                <select type="text "name="type" id="type">
                     <option value=""> - Kies je type - </option>
-                    <option value="achtbaan">achtbaan</option>
+                    <option value="achtbaan">Achtbaan</option>
                     <option value="draaiend">Draaiend</option>
                     <option value="kinder">Kinder</option>
                     <option value="horeca">Horeca</option>
                     <option value="show">Show</option>
                     <option value="water">Water</option>
                     <option value="overig">Overig</option>
+                </select>
             </div>
+
             
             <div class="form-group">
                 <label for="capaciteit">Capaciteit p/uur:</label>
-                <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
+                <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input" placeholder="0">
             </div>
+
             <div class="form-group">
                 <label for="melder">Naam melder:</label>
-                <input type="text" name="melder" id="melder" class="form-input">
+                <input type="text" name="melder" id="melder" class="form-input" placeholder="Pietje">
             </div>
+
 
             <div class="form-group">
                 <label for="prioriteit">Prioriteit:</label>
                 <input type="checkbox" name="newsletter" id="prioriteit">
                 <label for="prioriteit">Is dit een prioriteit?</label>
+            </div>
+
+            <div class="form-group">
+                <label for="overig">Overige info:</label>
+                <textarea name="overig" id="overig" class="form-input" rows="4"></textarea>
             </div>
         
 
